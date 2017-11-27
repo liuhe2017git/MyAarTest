e0450e76e9195f03784f25f54c72cc7f44a5ba06
liuhe2017git <545481180@qq.com>
2017-11-27 17:42:09 +0800

    Initial commit

---
+++ app/src/main/java/com/bwie/myaartest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.bwie.myaartest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bwie.myaar.AddUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println(AddUtil.add(1,5));
+
+    }
+}
